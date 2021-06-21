4b9941ded15312e0833de71cc623e623c3620498
Warat Wongmaneekit (Thang) <canopybanj@gmail.com>
2021-06-21 12:52:13 +0700

    Split member form

--- components/MemberForm.vue
+++ components/MemberForm.vue
@@ -1,118 +1,18 @@
 
 <template>
-  <el-form
-    label-width="100px"
-    :model="formData"
-    :label-position="'left'"
-    @submit.native.prevent="formSubmit"
-    :rules="rules"
-    ref="member-form"
-  >
-    <el-form-item label="Username" prop="username">
-      <el-input
-        placeholder="Username"
-        v-model="formData.username"
-        :disabled="mode === 'edit'"
-      ></el-input>
-    </el-form-item>
-    <el-form-item label="Name" prop="name">
-      <el-input placeholder="Name" v-model="formData.name"></el-input>
-    </el-form-item>
-    <el-form-item label="Email" prop="email">
-      <el-input placeholder="Email" v-model="formData.email"></el-input>
-    </el-form-item>
-    <el-form-item label="Password" prop="password" v-if="mode === 'create'">
-      <el-input
-        placeholder="Password"
-        v-model="formData.password"
-        show-password
-      ></el-input>
-    </el-form-item>
-    <el-button type="success" native-type="submit">Save</el-button>
-  </el-form>
+
 </template>
 
 <script>
 export default {
   name: "MemberForm",
   props: {
-    mode: {
-      tyep: String,
-      default: "create",
-    },
-    error: {
-      type: Boolean,
-    },
-    formData: {
-      type: Object,
-      default: () => {
-        return {
-          username: "",
-          password: "",
-          name: "",
-          email: "",
-        };
-      },
-    },
   },
   data() {
-    return {
-      rules: {
-        // TODO : Complete form rule
-        name: [
-          {
-            required: true,
-            message: "Name is required",
-            trigger: "blur",
-          }
-        ],
-        username: [
-          {
-            required: true,
-            message: "Username is required",
-            trigger: "blur",
-          }
-        ],
-        email: [
-          {
-            validator: (rule, value, callback) => {
-              const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-              if(!value) {
-                callback(new Error('Email is required'))
-              }
-
-              if(!re.test(String(value).toLowerCase())) {
-                callback(new Error('Email is not valid'))
-              } else {
-                callback()
-              }
-            },
-            trigger: "blur",
-          },
-        ],
-        password: [
-          {
-            required: true,
-            message: "Password is required",
-            trigger: "blur",
-          },
-        ],
-      },
-    };
   },
   methods: {
-    formSubmit() {
-      // TODO : Emit Event when form complete
-      this.$refs['member-form'].validate((valid) => {
-        if(valid) {
-          this.$emit("submit-form", this.formData);
-        } else {
-          return false;
-        }
-      })
 
-    },
   },
 };
 </script>
--- pages/member/Add.vue
+++ pages/member/Add.vue
@@ -3,7 +3,36 @@
     <el-col :span="6" :offset="9">
       <h1 class="header">Add new member</h1>
       <!-- TODO : Set event listener for save  -->
-      <MemberForm :formData="form" @submit-form="saveMember" :error="memberAddingError"></MemberForm>
+        <el-form
+        label-width="100px"
+        :model="formData"
+        :label-position="'left'"
+        @submit.native.prevent="formSubmit"
+        :rules="rules"
+        ref="member-form"
+      >
+        <el-form-item label="Username" prop="username">
+          <el-input
+            placeholder="Username"
+            v-model="formData.username"
+            :disabled="mode === 'edit'"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="Name" prop="name">
+          <el-input placeholder="Name" v-model="formData.name"></el-input>
+        </el-form-item>
+        <el-form-item label="Email" prop="email">
+          <el-input placeholder="Email" v-model="formData.email"></el-input>
+        </el-form-item>
+        <el-form-item label="Password" prop="password" v-if="mode === 'create'">
+          <el-input
+            placeholder="Password"
+            v-model="formData.password"
+            show-password
+          ></el-input>
+        </el-form-item>
+        <el-button type="success" native-type="submit">Save</el-button>
+      </el-form>
     </el-col>
   </el-row>
 </template>
