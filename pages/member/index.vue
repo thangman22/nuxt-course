14f2b99ff80724f95ff73f9a21ee90ebd00848e8
nakkrawee kuenkleep <nakkrawee@livingmobile.me>
2021-07-09 12:14:47 +0700

    Add edit and delete user function , pagination

--- pages/member/index.vue
+++ pages/member/index.vue
@@ -1,6 +1,6 @@
 <template>
   <el-col :span="16" :offset="4">
-    <!-- <el-row class="header">
+    <el-row class="header">
       <el-col :span="20"><h1>Member</h1></el-col>
       <el-col :span="4"
         ><el-button type="success" @click="$router.push({ name: 'member-add' })"
@@ -8,9 +8,8 @@
         ></el-col
       >
     </el-row>
-    <no-ssr> -->
-      <!-- TODO : Add no ssr for not render in Server  -->
-      <!-- <data-tables :data="users" :total="10">
+    <no-ssr>
+      <el-table :data="displayData" style="width: 100%">
         <div slot="empty" style="color: red">Users is empty</div>
         <el-table-column prop="username" label="Username" width="200" sortable>
         </el-table-column>
@@ -21,20 +20,71 @@
         <el-table-column fixed="right">
           <template slot-scope="scope">
             <el-button type="text" size="small" @click="editUser(scope.row)">Edit</el-button>
-            <el-button type="text" size="small">Delete</el-button>
+            <el-button type="text" size="small" @click="deleteUser(scope.row)">Delete</el-button>
           </template>
         </el-table-column>
-      </data-tables>
-    </no-ssr> -->
+        </el-table>
+        <el-pagination 
+          layout=" sizes, prev, pager, next, jumper , total" 
+          :total="this.users.length" 
+          @current-change="setPageNumber"
+          @size-change="changeLimitPerPage">
+        </el-pagination>
+    </no-ssr>
   </el-col>
 </template>
 
 <script>
 export default {
+  data(){
+    return{
+      users : [],
+        page : 1,
+        pageSize: 10,
+        items:'',
+        filtered :[],
+        search:'',
+    }
+  },
+  computed:{
+    displayData() {
+       if(this.search == null) return this.categories;
+        this.filtered = this.users.filter(data => !this.search || data.name.toLowerCase().includes(this.search.toLowerCase()));
+        return this.filtered.slice(this.pageSize * this.page - this.pageSize, this.pageSize * this.page);
+    }
+  },
   methods: {
+    // logEvent(d){
+    //   console.log(d)
+    //   const userRequest = await $axios.get('/api/users')
+    //   this.users = userRequest.data.data
+    // }
+    editUser(userInfo){
+      console.log(userInfo);
+      this.$router.push({name: 'member-edit-id', 
+      params:{
+          id: userInfo.username
+      }})
+    },
+    setPageNumber (val) {
+      this.page = val
+    },
+    changeLimitPerPage(val){
+      this.pageSize = val
+      this.displayData
+    },
+    async deleteUser(userInfo){
+      const statusCode = await this.$axios.delete(`/api/users/${userInfo.username}`)
+      
+    }
   },
   async asyncData({ $axios }) {
+    const userRequest = await $axios.get('/api/users')
+    return {users: userRequest.data.data}
   },
+  mounted(){
+    console.log(this.users)
+  }
 };
 </script>
 <style scoped>
--- server/index.mjs
+++ server/index.mjs
@@ -7,13 +7,10 @@ import cookieParser from 'cookie-parser';
 import crypto from 'crypto'
 const app = express();
 const jwtSecret = 'living1234';
-
 app.use(cors());
 app.use(express.json())
-
 app.post('/api/auth', async (req, res) => {
-    const apiRequest = await axios.get('https://living-mobile-demo-default-rtdb.asia-southeast1.firebasedatabase.app/users_list/' + req.body.username + '.json')
-
+    const apiRequest = await axios.get('https://workshopnuxtjs-default-rtdb.asia-southeast1.firebasedatabase.app/users_list/' + req.body.username + '.json')
     if(apiRequest.data.pwd === crypto.pbkdf2Sync(req.body.password.toString(), jwtSecret,  1000, 64, `sha512`).toString(`hex`)) {
       // TODO: Create JWT and set to cookie
       const token = jsonwebtoken.sign({ user: req.body.username }, jwtSecret);
@@ -22,30 +19,23 @@ app.post('/api/auth', async (req, res) => {
     } else {
       res.status(400).json({ status: 'Auth Error' });
     }
-
 });
 app.use(cookieParser());
 // TODO: Check JWT with every request
 app.use(jwt({ secret: jwtSecret, algorithms: ['HS256'], getToken: req => req.cookies.token }));
-
 app.get('/api/users/', async (req, res) => {
   let offest = 0
   let max = 20
-  const apiRequest = await axios.get('https://living-mobile-demo-default-rtdb.asia-southeast1.firebasedatabase.app/users_list.json')
-
+  const apiRequest = await axios.get('https://workshopnuxtjs-default-rtdb.asia-southeast1.firebasedatabase.app/users_list.json')
   let returnData = []
-
   for(const index in apiRequest.data) {
     apiRequest.data[index]._id = index
     delete apiRequest.data[index].pwd
     returnData.push(apiRequest.data[index])
   }
-
   returnData = returnData.splice(offest, max)
-
   res.json({ status: "success", data: returnData });
 });
-
 app.post('/api/users', async (req, res) => {
     const user = {
       username: req.body.username,
@@ -53,25 +43,25 @@ app.post('/api/users', async (req, res) => {
       email: req.body.email,
       pwd: crypto.pbkdf2Sync(req.body.password.toString(), jwtSecret,  1000, 64, `sha512`).toString(`hex`)
     }
-
-    const apiRequest = await axios.put('https://living-mobile-demo-default-rtdb.asia-southeast1.firebasedatabase.app/users_list/'+ req.body.username + '.json', user)
+    const apiRequest = await axios.put('https://workshopnuxtjs-default-rtdb.asia-southeast1.firebasedatabase.app/users_list/'+ req.body.username + '.json', user)
     res.json({ status: "success", data: apiRequest.data });
 });
-
 app.put('/api/users', async (req, res) => {
   const user = {
     email: req.body.email,
     name: req.body.name,
+    username: req.body.username
   }
-
-  const apiRequest = await axios.put('https://living-mobile-demo-default-rtdb.asia-southeast1.firebasedatabase.app/users_list/'+ req.body.username + '.json', user)
+  const apiRequest = await axios.put('https://workshopnuxtjs-default-rtdb.asia-southeast1.firebasedatabase.app/users_list/'+ req.body.username + '.json', user)
   res.json({ status: "success", data: apiRequest.data });
 });
-
 app.get('/api/users/:id', async (req, res) => {
-  const apiRequest = await axios.get('https://living-mobile-demo-default-rtdb.asia-southeast1.firebasedatabase.app/users_list/' + req.params.id + '.json')
+  const apiRequest = await axios.get('https://workshopnuxtjs-default-rtdb.asia-southeast1.firebasedatabase.app/users_list/' + req.params.id + '.json')
   res.json({ status: "success", data: apiRequest.data });
 });
-
+app.delete('/api/users/:id', async(req, res) => {
+  const apiRequest = await axios.delete('https://workshopnuxtjs-default-rtdb.asia-southeast1.firebasedatabase.app/users_list/' + req.params.id + '.json')
+  res.json({ status: "success", data: apiRequest.data });
+});
 app.listen(3001);
 console.log('Api running on http://localhost:3001');
